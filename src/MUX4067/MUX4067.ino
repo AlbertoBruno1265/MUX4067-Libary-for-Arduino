10ceea260433cb3fa8c73d8b48460e03a291e85d
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2023-09-04 21:36:48 -0300

    Update MUX4067.ino

--- src/MUX4067/MUX4067.ino
+++ src/MUX4067/MUX4067.ino
@@ -1,6 +1,6 @@
 #include "MUX4067.h"
 
-mux4067 mux(A0, 14, 12, 13, 15, 0, 0);
+mux4067 mux(2, 14, 12, 13, 15, 1, 1);
 
 int sensor0;
 
@@ -9,7 +9,10 @@ void setup() {
 }
 
 void loop() {
-  sensor0 = mux.c0();
-  Serial.print("Sensor0: "); Serial.println(sensor0);
+//  sensor0 = mux.c0();
+//  Serial.print("Sensor0: "); Serial.println(sensor0);
+  sensor0 = mux.c1(HIGH);
+  delay(1000);
+  sensor0 = mux.c1(LOW);
   delay(1000);
 }
