81a7b0e5f09e3e130356fd4336e9c16d06751ea0
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2023-09-05 15:29:17 -0300

    Create c14 method

--- src/MUX4067/MUX4067.cpp
+++ src/MUX4067/MUX4067.cpp
@@ -182,3 +182,12 @@ int mux4067::c13(bool sig_state, int sig_pwm){
   
   return mux4067::_check_choice(sig_state, sig_pwm);
 }
+
+int mux4067::c14(bool sig_state, int sig_pwm){
+  digitalWrite(_s0, LOW);
+  digitalWrite(_s1, HIGH);
+  digitalWrite(_s2, HIGH);
+  digitalWrite(_s3, HIGH);
+  
+  return mux4067::_check_choice(sig_state, sig_pwm);
+}
--- src/MUX4067/MUX4067.ino
+++ src/MUX4067/MUX4067.ino
@@ -1,7 +1,7 @@
 #include "MUX4067.h"
 
 // Test Input Analog
-//mux4067 mux(14, 12, 13, 15, A0);
+mux4067 mux(14, 12, 13, 15, A0);
 
 // Test Input Digital
 //mux4067 mux(14, 12, 13, 15, A0, 0, 1);
@@ -10,7 +10,7 @@
 //mux4067 mux(14, 12, 13, 15, 2, 1);
 
 // Test Output Digital
-mux4067 mux(14, 12, 13, 15, 2, 1, 1);
+//mux4067 mux(14, 12, 13, 15, 2, 1, 1);
 
 int sensor0;
 
@@ -24,10 +24,10 @@ void loop() {
 //  Serial.print("Sensor0: "); Serial.println(sensor0);
 
   // Test Chennel 1
-  mux.c13(HIGH);
-  delay(1000);
-  mux.c13(LOW);
-  delay(1000);
+//  mux.c14(HIGH);
+//  delay(1000);
+//  mux.c14(LOW);
+//  delay(1000);
 
   // Test Chennel 2
 //  sensor0 = mux.c2();
@@ -76,4 +76,8 @@ void loop() {
     //     Test Chennel 13
 //  sensor0 = mux.c13();
 //  Serial.print("Sensor0: "); Serial.println(sensor0);
+
+    //     Test Chennel 13
+  sensor0 = mux.c14();
+  Serial.print("Sensor0: "); Serial.println(sensor0);
 }
