e7928ac792c560bcce16f30266a334a2d6e58a14
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2023-09-05 15:11:51 -0300

    Create c10 method

--- src/MUX4067/MUX4067.cpp
+++ src/MUX4067/MUX4067.cpp
@@ -146,3 +146,12 @@ int mux4067::c9(bool sig_state, int sig_pwm){
   
   return mux4067::_check_choice(sig_state, sig_pwm);
 }
+
+int mux4067::c10(bool sig_state, int sig_pwm){
+  digitalWrite(_s0, LOW);
+  digitalWrite(_s1, HIGH);
+  digitalWrite(_s2, LOW);
+  digitalWrite(_s3, HIGH);
+  
+  return mux4067::_check_choice(sig_state, sig_pwm);
+}
