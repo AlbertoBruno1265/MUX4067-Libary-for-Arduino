068803bb21f1646524e720e32fb3c7f456366dd7
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2023-09-06 18:56:39 -0300

    Update All_Channels_Values.ino

--- examples/All_Channels_Values/All_Channels_Values.ino
+++ examples/All_Channels_Values/All_Channels_Values.ino
@@ -2,15 +2,16 @@
 
 mux4067 mux(14, 12, 13, 15, A0);
 
-int* values_arr[16];
+int values_arr[16];
 
 void setup(){
   Serial.begin(115200);
 }
 
 void loop(){
-    for (int i=0; i<16; i++){
-      Serial.print("Channel "); Serial.print(i); Serial.print(": "); Serial.println(mux.all_channels_value()[i]);
-      delay(1000);
-    }
+  for (int i=0; i<16; i++){
+    values_arr[i] = int(mux.all_channels_value()[i]);
+    Serial.print("Channel "); Serial.print(i); Serial.print(": "); Serial.println(values_arr[i]);
+    delay(500);
+  }
 }
